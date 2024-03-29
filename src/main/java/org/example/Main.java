9ed38f1c23614ab6481280e77e40978029d90099
Michał Dydek <dydmichal@gmail.com>
2024-03-29 18:25:54 +0100

    basic case string

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,30 +7,35 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-        if (args.length != 1) {
+        if (args.length > 1) {
             System.out.println("USAGE -> .\\gradlew.bat run --args \"<name_of_file>\"");
             return;
         }
-        String filePath = args[0]; // Path to the JSON file
-
-        try (FileReader fileReader = new FileReader(filePath)) {
-            JsonObject jsonObject;
-            try{
-                jsonObject = JsonParser.parseReader(fileReader).getAsJsonObject();
-            } catch (JsonSyntaxException e){
-                System.out.println("WRONG JSON SYNTAX");
-                return;
-            }
-            if (jsonObject != null){
+        JsonObject jsonObject = null;
+        if (args.length == 1){
+            String filePath = args[0];
+            try (FileReader fileReader = new FileReader(filePath)) {
                 try {
-                    System.out.println(Main.VerifyJson(jsonObject));
-                } catch (WrongJsonSyntaxException e){
-                    System.out.println(e.getMessage());
+                    jsonObject = JsonParser.parseReader(fileReader).getAsJsonObject();
+                } catch (JsonSyntaxException e) {
+                    System.out.println("WRONG JSON SYNTAX");
+                    return;
                 }
-
+            } catch (IOException e) {
+                System.out.println("error opening a file");
             }
-        } catch (IOException e) {
-            System.out.println("error opening a file");
+        }
+        else{
+            String policyJson = "{\"PolicyName\":\"root\",\"PolicyDocument\":{\"Version\":\"2012-10-17\",\"Statement\":[{\"Sid\":\"IamListAccess\",\"Effect\":\"Allow\",\"Action\":[\"iam:ListRoles\",\"iam:ListUsers\"],\"Resource\":\"*\"}]}}";
+            jsonObject = JsonParser.parseString(policyJson).getAsJsonObject();
+        }
+        if (jsonObject != null) {
+            try {
+                System.out.println(Main.VerifyJson(jsonObject));
+            } catch (WrongJsonSyntaxException e) {
+                System.out.println(e.getMessage());
+            }
+
         }
     }
 
